f5177822836afce21de3360bd037674ebe57a1e1
A_noob_in_coding <naeemabdulrehman77@gmail.com>
2024-08-07 23:13:59 +0500

    added the styles and logic for page in styles.css and scipt.js respectively

---
+++ script.js
@@ -0,0 +1,38 @@
+let email_input = document.getElementById("email-input");
+let dismiss_btn = document.getElementById("dismiss-btn")
+let submit_btn = document.getElementById("submit-btn")
+let main = document.querySelector("main")
+let success_container = document.getElementById("success-container");
+let email = ""
+let error_el = document.getElementById("error")
+let span_email = document.getElementById("user_email")
+// event listeners
+submit_btn.addEventListener("click", () =>{
+    if( validateEmail(email)){
+        main.style.display = "none"
+        success_container.style.display = "flex"
+        span_email.innerText = email
+        email_input.style.borderColor = "black"
+        error_el.style.display = "none"
+    }
+    else{
+        email_input.style.borderColor = "red"
+        error_el.style.display = "block"
+    }
+})
+
+dismiss_btn.addEventListener("click", ()=>{
+    main.style.display = "flex"
+    success_container.style.display = "none"
+})
+
+email_input.addEventListener("input" , ()=>{
+    email = email_input.value
+})
+
+//functions
+function validateEmail(email) {
+    // Regular expression for validating an email address
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
